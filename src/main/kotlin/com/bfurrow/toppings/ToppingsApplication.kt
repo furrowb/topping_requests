172580233de2d9430263878ec80175e803136b29
Beau Furrow <5bdmh7mr1@relay.firefox.com>
2023-02-28 15:22:56 -0500

    Initial commit after Spring Initializr

---
+++ src/main/kotlin/com/bfurrow/toppings/ToppingsApplication.kt
@@ -0,0 +1,11 @@
+package com.bfurrow.toppings
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ToppingsApplication
+
+fun main(args: Array<String>) {
+	runApplication<ToppingsApplication>(*args)
+}
---
+++ src/test/kotlin/com/bfurrow/toppings/ToppingsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bfurrow.toppings
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ToppingsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
