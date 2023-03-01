ae5c89133a1872505b1046ecd620ca3f29c55492
Beau Furrow <5bdmh7mr1@relay.firefox.com>
2023-03-01 02:48:42 -0500

    Update comment on Jooq + SQLite's date support

--- src/main/resources/db/migration/V1__initial_setup.sql
+++ src/main/resources/db/migration/V1__initial_setup.sql
@@ -1,13 +1,13 @@
 CREATE TABLE users(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     email TEXT UNIQUE NOT NULL,
-    created_date TEXT NOT NULL -- SQLite doesn't have great date support so we make it a text field instead
+    created_date TEXT NOT NULL -- SQLite + Jooq doesn't have great date support so we make it a text field instead
 );
 
 CREATE TABLE requested_toppings(
     user_id INTEGER NOT NULL,
     topping TEXT NOT NULL,
-    created_date TEXT NOT NULL, -- SQLite doesn't have great date support so we make it a text field instead
+    created_date TEXT NOT NULL, -- SQLite + Jooq doesn't have great date support so we make it a text field instead
     FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
