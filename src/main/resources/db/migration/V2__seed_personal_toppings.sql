d0ad2522b6d27c0e51dc4adb0a1af0d8c42a9293
Beau Furrow <5bdmh7mr1@relay.firefox.com>
2023-03-01 13:04:21 -0500

    Add seed migration for personal topping preference

---
+++ src/main/resources/db/migration/V2__seed_personal_toppings.sql
@@ -0,0 +1,9 @@
+INSERT INTO users(id, email, created_date) VALUES(1, 'resume@bfurrow.com', '1677646800000');
+
+-- Chicken Pesto Pizza from a local bakery
+INSERT INTO requested_toppings (user_id, topping, created_date) VALUES
+    (1, 'chicken', '1677646800000'),
+    (1, 'basil pesto', '1677646800000'),
+    (1, 'red peppers', '1677646800000'),
+    (1, 'mozzarella', '1677646800000'),
+    (1, 'pine nuts', '1677646800000');
