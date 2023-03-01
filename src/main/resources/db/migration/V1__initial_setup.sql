5df78fbe65e69d9683cd335e65ab0db1eaa59dc4
Beau Furrow <5bdmh7mr1@relay.firefox.com>
2023-03-01 02:45:37 -0500

    Add foreign key to toppings table

--- src/main/resources/db/migration/V1__initial_setup.sql
+++ src/main/resources/db/migration/V1__initial_setup.sql
@@ -7,7 +7,8 @@ CREATE TABLE users(
 CREATE TABLE requested_toppings(
     user_id INTEGER NOT NULL,
     topping TEXT NOT NULL,
-    created_date TEXT NOT NULL -- SQLite doesn't have great date support so we make it a text field instead
+    created_date TEXT NOT NULL, -- SQLite doesn't have great date support so we make it a text field instead
+    FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
 CREATE UNIQUE INDEX unique_user_topping ON requested_toppings(user_id, topping);
